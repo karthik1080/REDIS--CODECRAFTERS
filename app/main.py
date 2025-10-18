ba2b33c144c7cc3a0e90f2657517676a95cb1f39
karthik1080 <karthik.tenneti.2005@gmail.com>
2025-10-18 17:59:00 +0530

    PING

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, x= server_socket.accept() # wait for client
-    print (connection, x)
-    connection.sendall(b"+PONG\r\n")
+    data = connection.recv(1024).decode()
+    for i in range(0,len(data)):
+        if i+4 < len(data) and data[i:i+4] == 'PING':
+            connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
