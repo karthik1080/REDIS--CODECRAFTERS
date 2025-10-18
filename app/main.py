49590cf6715b095a3527b83bb0f607b2d972ca20
karthik1080 <karthik.tenneti.2005@gmail.com>
2025-10-18 17:34:47 +0530

    PING

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, x server_socket.accept() # wait for client
+    connection, x= server_socket.accept() # wait for client
     print (connection, x)
     connection.sendall(b"+PONG\r\n")
 
