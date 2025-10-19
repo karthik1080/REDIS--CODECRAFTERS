9e3be582b855103710867516c72e98a857a56260
karthik1080 <karthik.tenneti.2005@gmail.com>
2025-10-19 17:18:14 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def handle_command(client: socket.socket):
         if chunk == b"":
             break
         print(f"[CHUNK] ```\n{chunk.decode()}\n```")
-        client.sendall(b"+PONG\r\n")
+        print(f"Received data: {chunk}")
+        if chunk.startswith(b"*1\r\n$4\r\nPING\r\n"):
+            client.sendall(b"+PONG\r\n")
+        if chunk.startswith(b"*2\r\n$4\r\nECHO\r\n"):
+            msg = chunk.split(b"\r\n")[-2]
+            client.sendall(b"$" + str(len(msg)).encode() + b"\r\n" + msg + b"\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
